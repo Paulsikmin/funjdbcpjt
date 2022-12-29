bae2369574d5b837a66fd3be9796e5d52c1f6bcd
user1 <user1@DESKTOP-SE4JILN>
2022-12-29 12:40:29 +0900

    ADD : Day02 학생관리 기능 추가
    1. 아이디로 조회 기능 완료
    2. 이름으로 조회 기능 완료(List)
    3. 삭제 기능 완료
    4. 수정 기능 완료

--- src/com/kh/jdbc/day01/student/controller/StudentController.java
+++ src/com/kh/jdbc/day01/student/controller/StudentController.java
@@ -6,16 +6,69 @@ import com.kh.jdbc.day01.student.model.dao.StudentDAO;
 import com.kh.jdbc.day01.student.model.vo.Student;
 
 public class StudentController {
-
+	/**
+	 * 학생 전체 목록 조회
+	 * @return List<Student>
+	 */
 	public List<Student> printAll() {
 		StudentDAO sDao = new StudentDAO();
 		List<Student> sList = sDao.selectAll();
 		return sList;
 	}
-	
+	/**
+	 * 학생 이름으로 조회
+	 * @param studentName
+	 * @return List<Student>
+	 */
+	public List<Student> printAllByName(String studentName) {
+		StudentDAO sDao = new StudentDAO();
+		List<Student> sList = sDao.selectAllByName(studentName);
+		return sList;
+	}
+	/**
+	 * 학생 아이디로 조회
+	 * @param studentId
+	 * @return Student
+	 */
+	public Student printOneById(String studentId) {
+		StudentDAO sDao = new StudentDAO();
+		Student student = sDao.selectOneById(studentId);
+		return student;
+	}
+	/**
+	 * 
+	 * @param student
+	 * @return
+	 */
 	public int registerStudent(Student student) {
 		StudentDAO sDao = new StudentDAO();
 		int result = sDao.insertMember(student);
 		return result;
 	}
+	/**
+	 * 
+	 * @param studentId
+	 */
+	public int removeStudent(String studentId) {
+		StudentDAO sDao = new StudentDAO();
+		int result = sDao.deleteMember(studentId);
+		return result;
+		// Void methods cannot return a value
+	}
+	/**
+	 * 
+	 * @param student
+	 * @return
+	 */
+	public int modifyStudent(Student student) {
+		StudentDAO sDao = new StudentDAO();
+		int result = sDao.updateStudent(student);
+		return result;
+	}
+	
+	
+	
+	
+	
+	
 }
