a195a40e7347e4ba012d13635fb6b1132d980256
user1 <user1@DESKTOP-SE4JILN>
2023-01-03 11:27:30 +0900

    FEAT : Day05 회원관리 프로그램 완료
    1. JDBCTemplate Connection 싱글톤 수정
    2. 회원 탈퇴 기능 완료
    3. 회원 이름으로 조회 기능 완료
    4. 회원 아이디로 조회 기능 완료

--- src/com/kh/jdbc/day03/member/model/dao/MemberDAO.java
+++ src/com/kh/jdbc/day03/member/model/dao/MemberDAO.java
@@ -14,7 +14,7 @@ import com.kh.jdbc.day03.member.model.vo.Member;
 public class MemberDAO {
 
 	/**
-	 * 전체 회원 정보 조회
+	 * 전체 회원 정보 조회 DAO
 	 * @param conn
 	 * @return
 	 */
@@ -45,65 +45,48 @@ public class MemberDAO {
 		}
 		return mList;
 	}
-	
 	/**
-	 * 회원 정보 등록
+	 * 회원 이름으로 조회 DAO
 	 * @param conn
-	 * @param member
-	 * @return
+	 * @param memberName
+	 * @return List<Member>
 	 */
-	public int insertMember(Connection conn, Member member) {
-		int result = 0;
-		String sql 
-		= "INSERT INTO MEMBER_TBL VALUES(?,?,?,?,?,?,?,?,?,DEFAULT)";
+	public List<Member> selectAllByName(Connection conn, String memberName) {
+		String sql = "SELECT * FROM MEMBER_TBL WHERE MEMBER_NAME LIKE ?";
+		List<Member> mList = null;
 		try {
 			PreparedStatement pstmt = conn.prepareStatement(sql);
-			pstmt.setString(1, member.getMemberId());
-			pstmt.setString(2, member.getMemberPwd());
-			pstmt.setString(3, member.getMemberName());
-			pstmt.setString(4, member.getMemberGender());
-			pstmt.setInt(5, member.getMemberAge());
-			pstmt.setString(6, member.getMemberEmail());
-			pstmt.setString(7, member.getMemberPhone());
-			pstmt.setString(8, member.getMemberAddress());
-			pstmt.setString(9, member.getMemberHobby()); // 쿼리문 실행 준비완료
-			result = pstmt.executeUpdate();	// 쿼리문 실행
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-		return result;
-	}
-	/**
-	 * 회원 정보 수정 
-	 * @param conn
-	 * @param member
-	 * @return
-	 */
-	public int updateMember(Connection conn, Member member) {	// 회원 정보 수정
-		// UPDATE MEMBER_TBL SET MEMBER_PWD = ?, 
-		// MEMBER_EMAIL = ?, MEMBER_PHONE = ?, MEMBER_ADDRESS = ?, MEMBER_HOBBY = ? WHERE MEMBER_ID = ?
-		
-		// Class.forName(DRIVER_NAME);
-		// Connection conn = DriverManager.getConnection(URL, USER, PASSWORD);
-		String sql = "UPDATE MEMBER_TBL SET MEMBER_PWD = ?, MEMBER_EMAIL = ?, MEMBER_PHONE = ?, MEMBER_ADDRESS = ?, MEMBER_HOBBY = ? WHERE MEMBER_ID = ?";
-		int result = 0;
-		try {
-			PreparedStatement pstmt = conn.prepareStatement(sql);
-			pstmt.setString(1, member.getMemberPwd());
-			pstmt.setString(2, member.getMemberEmail());
-			pstmt.setString(3, member.getMemberPhone());
-			pstmt.setString(4, member.getMemberAddress());
-			pstmt.setString(5, member.getMemberHobby());
-			pstmt.setString(6, member.getMemberId());	// 쿼리문 실행 준비 완료
-			result = pstmt.executeUpdate();				// 쿼리문 실행
-			pstmt.close();
+			pstmt.setString(1, "%"+memberName+"%");
+			ResultSet rset = pstmt.executeQuery();
+			mList = new ArrayList<Member>();
+			while(rset.next()) {
+				// 후처리
+				Member member = new Member();
+				member.setMemberId(rset.getString("MEMBER_ID"));
+				member.setMemberPwd(rset.getString("MEMBER_PWD"));
+				member.setMemberName(rset.getString("MEMBER_NAME"));
+				member.setMemberAge(rset.getInt("MEMBER_AGE"));
+				member.setMemberEmail(rset.getString("MEMBER_EMAIL"));
+				member.setMemberPhone(rset.getString("MEMBER_PHONE"));
+				member.setMemberGender(rset.getString("MEMBER_GENDER"));
+				member.setMemberHobby(rset.getString("MEMBER_HOBBY"));
+				member.setMemberAddress(rset.getString("MEMBER_ADDRESS"));
+				member.setMemberDate(rset.getTimestamp("MEMBER_DATE"));
+				mList.add(member);
+			}
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		return result;
+		return mList;
 	}
 
+	/**
+	 * 아이디로 조회 DAO
+	 * @param conn
+	 * @param memberId
+	 * @return
+	 */
 	public Member selectOneById(Connection conn, String memberId) {
 		String sql = "SELECT * FROM MEMBER_TBL WHERE MEMBER_ID = ?";
 		Member member = null;
@@ -132,8 +115,84 @@ public class MemberDAO {
 		}
 		return member;
 	}
-	
-	
+
+	/**
+	 * 회원 정보 등록 DAO
+	 * @param conn
+	 * @param member
+	 * @return
+	 */
+	public int insertMember(Connection conn, Member member) {
+		int result = 0;
+		String sql 
+		= "INSERT INTO MEMBER_TBL VALUES(?,?,?,?,?,?,?,?,?,DEFAULT)";
+		try {
+			PreparedStatement pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, member.getMemberId());
+			pstmt.setString(2, member.getMemberPwd());
+			pstmt.setString(3, member.getMemberName());
+			pstmt.setString(4, member.getMemberGender());
+			pstmt.setInt(5, member.getMemberAge());
+			pstmt.setString(6, member.getMemberEmail());
+			pstmt.setString(7, member.getMemberPhone());
+			pstmt.setString(8, member.getMemberAddress());
+			pstmt.setString(9, member.getMemberHobby()); // 쿼리문 실행 준비완료
+			result = pstmt.executeUpdate();	// 쿼리문 실행
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return result;
+	}
+	/**
+	 * 회원 정보 수정  DAO
+	 * @param conn
+	 * @param member
+	 * @return
+	 */
+	public int updateMember(Connection conn, Member member) {	// 회원 정보 수정
+		// UPDATE MEMBER_TBL SET MEMBER_PWD = ?, 
+		// MEMBER_EMAIL = ?, MEMBER_PHONE = ?, MEMBER_ADDRESS = ?, MEMBER_HOBBY = ? WHERE MEMBER_ID = ?
+		
+		// Class.forName(DRIVER_NAME);
+		// Connection conn = DriverManager.getConnection(URL, USER, PASSWORD);
+		String sql = "UPDATE MEMBER_TBL SET MEMBER_PWD = ?, MEMBER_EMAIL = ?, MEMBER_PHONE = ?, MEMBER_ADDRESS = ?, MEMBER_HOBBY = ? WHERE MEMBER_ID = ?";
+		int result = 0;
+		try {
+			PreparedStatement pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, member.getMemberPwd());
+			pstmt.setString(2, member.getMemberEmail());
+			pstmt.setString(3, member.getMemberPhone());
+			pstmt.setString(4, member.getMemberAddress());
+			pstmt.setString(5, member.getMemberHobby());
+			pstmt.setString(6, member.getMemberId());	// 쿼리문 실행 준비 완료
+			result = pstmt.executeUpdate();				// 쿼리문 실행
+			pstmt.close();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return result;
+	}
+	/**
+	 * 회원 탈퇴 DAO
+	 * @param conn
+	 * @param memberId
+	 * @return
+	 */
+	public int deleteMember(Connection conn, String memberId) {
+		String sql = "DELETE FROM MEMBER_TBL WHERE MEMBER_ID = ?";
+		int result = 0;
+		try {
+			PreparedStatement pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, memberId); 	// 쿼리문 실행준비 완료
+			result = pstmt.executeUpdate();
+			pstmt.close();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return result;
+	}
 	
 	
 	
