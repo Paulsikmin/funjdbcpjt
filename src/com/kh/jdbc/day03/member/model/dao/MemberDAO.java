bcc63ce4103ec6ba51b0d66725e735a36a8ea927
user1 <user1@DESKTOP-SE4JILN>
2023-01-03 12:43:49 +0900

    FEAT : Properties 이용하여 연결정보 파일 관리
    1. file에 DB접속정보 관리
    2. Properties 이용하여 key-value형태로 사용

--- src/com/kh/jdbc/day03/member/model/dao/MemberDAO.java
+++ src/com/kh/jdbc/day03/member/model/dao/MemberDAO.java
@@ -1,5 +1,8 @@
 package com.kh.jdbc.day03.member.model.dao;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
@@ -8,10 +11,25 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Properties;
 
 import com.kh.jdbc.day03.member.model.vo.Member;
 
 public class MemberDAO {
+	
+	private Properties prop;
+	
+	public MemberDAO() {
+		prop = new Properties();
+		try {
+			FileReader reader = new FileReader("resources/query.properties");
+			prop.load(reader);
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 
 	/**
 	 * 전체 회원 정보 조회 DAO
@@ -20,7 +38,7 @@ public class MemberDAO {
 	 */
 	public List<Member> selectAll(Connection conn) { // 회원 전체 정보 조회
 		List<Member> mList = null;
-		String query = "SELECT * FROM MEMBER_TBL";
+		String query = prop.getProperty("selectAll");
 		try {
 			Statement stmt = conn.createStatement();
 			ResultSet rset = stmt.executeQuery(query);
@@ -52,7 +70,7 @@ public class MemberDAO {
 	 * @return List<Member>
 	 */
 	public List<Member> selectAllByName(Connection conn, String memberName) {
-		String sql = "SELECT * FROM MEMBER_TBL WHERE MEMBER_NAME LIKE ?";
+		String sql = prop.getProperty("selectAllByName");
 		List<Member> mList = null;
 		try {
 			PreparedStatement pstmt = conn.prepareStatement(sql);
@@ -88,7 +106,7 @@ public class MemberDAO {
 	 * @return
 	 */
 	public Member selectOneById(Connection conn, String memberId) {
-		String sql = "SELECT * FROM MEMBER_TBL WHERE MEMBER_ID = ?";
+		String sql = prop.getProperty("selectOneById");
 		Member member = null;
 		try {
 			PreparedStatement pstmt = conn.prepareStatement(sql);
@@ -124,8 +142,7 @@ public class MemberDAO {
 	 */
 	public int insertMember(Connection conn, Member member) {
 		int result = 0;
-		String sql 
-		= "INSERT INTO MEMBER_TBL VALUES(?,?,?,?,?,?,?,?,?,DEFAULT)";
+		String sql = prop.getProperty("insertMember");
 		try {
 			PreparedStatement pstmt = conn.prepareStatement(sql);
 			pstmt.setString(1, member.getMemberId());
@@ -155,7 +172,7 @@ public class MemberDAO {
 		
 		// Class.forName(DRIVER_NAME);
 		// Connection conn = DriverManager.getConnection(URL, USER, PASSWORD);
-		String sql = "UPDATE MEMBER_TBL SET MEMBER_PWD = ?, MEMBER_EMAIL = ?, MEMBER_PHONE = ?, MEMBER_ADDRESS = ?, MEMBER_HOBBY = ? WHERE MEMBER_ID = ?";
+		String sql = prop.getProperty("updateMember");
 		int result = 0;
 		try {
 			PreparedStatement pstmt = conn.prepareStatement(sql);
@@ -180,7 +197,7 @@ public class MemberDAO {
 	 * @return
 	 */
 	public int deleteMember(Connection conn, String memberId) {
-		String sql = "DELETE FROM MEMBER_TBL WHERE MEMBER_ID = ?";
+		String sql = prop.getProperty("deleteMember");
 		int result = 0;
 		try {
 			PreparedStatement pstmt = conn.prepareStatement(sql);
